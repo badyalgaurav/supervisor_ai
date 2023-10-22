34dd14f6bbc193b6d851cbd358e2e644363be60e
Garry <badyalgaurav0@gmail.com>
2023-10-22 13:47:10 +0900

    TesT

--- src/components/VideoModal.jsx
+++ src/components/VideoModal.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
+const customStyles = {
+    content: {
+        zIndex: 9999, // Set a high z-index value
+    },
+};
 
 const VideoModal = ({ isOpen, videoUrl, onRequestClose }) => {
     return (
@@ -10,6 +15,7 @@ const VideoModal = ({ isOpen, videoUrl, onRequestClose }) => {
             isOpen={isOpen}
             onRequestClose={onRequestClose}
             contentLabel="Video Modal"
+            style={customStyles} // Apply the custom styles
         >
             <video src={videoUrl} controls autoPlay />
             <button onClick={onRequestClose}>Close</button>
